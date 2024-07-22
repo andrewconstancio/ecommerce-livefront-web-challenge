52e49cb966e5821be85c767c0ad3e34cd886709b
Andrew Constancio <61609406+andrewconstancio@users.noreply.github.com>
2024-07-22 10:50:51 -0500

    misc fix
    misc fix

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
 	// return the main html content for this component
@@ -9,7 +10,7 @@ const HeroSection = () => {
 					<h2>BIGGEST SALE OF THE SEASON</h2>
 					<h2>Shop now to save big</h2>
 					<p>Explore the best quality electronics, jewelry, men's and women's clothing</p>
-					<a className="hero-section-show-now-link" href="">Shop Now</a>
+					<Link className="hero-section-show-now-link" href="">Shop Now</Link>
 				</div>
 			</div>
 		</section>
